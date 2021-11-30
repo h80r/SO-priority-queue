aaf9ec1489d2e59f5a33b7f095015e20cacfc4c2
h80r <h80rgbc@gmail.com>
2021-11-30 20:46:46 -0300

    Create Queue model

---
+++ bin/models/queue.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+import 'pcb.dart';
+
+class Queue {
+  Queue({required this.qid}) {
+    final rand = Random();
+
+    priority = rand.nextInt(10);
+    delta = rand.nextInt(5) + 1;
+  }
+
+  final int qid;
+  final List<PCB> _pcbs = [];
+  late final int priority;
+  late final int delta;
+
+  int currentPID = 0;
+
+  void addPCB(PCB newPCB) {
+    _pcbs.add(newPCB);
+  }
+
+  PCB? removePCB() {
+    if (_pcbs.isEmpty) return null;
+    return _pcbs.removeAt(0);
+  }
+
+  void run() {
+    if (_pcbs.isEmpty) return;
+    _pcbs[currentPID].run(delta);
+    currentPID = (currentPID + 1) % _pcbs.length;
+  }
+
+  @override
+  String toString() {
+    return 'Queue $qid (Priority: $priority, Delta: $delta)';
+  }
+}
