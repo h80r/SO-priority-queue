4c205b1963024397449a8170342ede5341951236
h80r <h80rgbc@gmail.com>
2021-11-30 23:39:16 -0300

    Add user input using Interact

--- bin/priority_queue.dart
+++ bin/priority_queue.dart
@@ -1,24 +1,51 @@
 import 'dart:io';
 import 'dart:math';
 
+import 'package:interact/interact.dart';
+
 import 'models/pcb.dart';
 import 'models/queue.dart';
 
 void main(List<String> arguments) async {
-  final queues = List.generate(5, (i) => Queue(i))
+  final queueCount = int.parse(Input(
+    prompt: 'Quantas filas você deseja?',
+    defaultValue: '5',
+    validator: (value) {
+      final inputInt = int.tryParse(value);
+      if (inputInt != null && inputInt >= 1 && inputInt <= 10) return true;
+      if (inputInt == null) throw ValidationError('Valor inválido');
+      if (inputInt < 1) throw ValidationError('Deve haver ao menos uma fila.');
+      throw ValidationError('Deve haver no máximo 10 filas.');
+    },
+  ).interact());
+
+  final queues = List.generate(queueCount, (i) => Queue(i))
     ..sort((prev, next) => next.priority.compareTo(prev.priority))
     ..forEach((q) => print(q));
 
-  var processesToInitialize = 20;
+  final processCount = int.parse(Input(
+    prompt: 'Quantos processos você deseja simular?',
+    defaultValue: '20',
+    validator: (value) {
+      final inputInt = int.tryParse(value);
+      if (inputInt != null && inputInt >= 5 && inputInt <= 30) return true;
+      if (inputInt == null) throw ValidationError('Valor inválido');
+      if (inputInt < 5) throw ValidationError('Simule ao menos 5 processos.');
+      throw ValidationError('Não é recomendável simular mais de 30 processos.');
+    },
+  ).interact());
+
+  var processesToInitialize = processCount;
+
   var lastValidAddress = 0;
 
   final _rand = Random();
 
   while (true) {
     if (processesToInitialize != 0) {
-      final selectedQueue = queues[_rand.nextInt(5)];
+      final selectedQueue = queues[_rand.nextInt(queueCount)];
       final newProcess = PCB(
-        pid: 20 - processesToInitialize,
+        pid: processCount - processesToInitialize,
         priority: selectedQueue.priority,
         startingAddress: lastValidAddress,
       );
